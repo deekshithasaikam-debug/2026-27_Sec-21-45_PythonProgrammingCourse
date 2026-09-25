237495157ba78ed06f72ebd7f2c59364d61bdda4
deekshithasaikam-debug <deekshithasaikam@gmail.com>
2026-09-25 16:04:00 +0530

    Add files via upload
    Unit-1_Task-15_Type_Conversion.py

---
+++ Unit-1_Task-15_Type_Conversion.py
@@ -0,0 +1,5 @@
+# Type Conversion
+type1 = int(input("Enter an integer: "))
+type2 = float(input("Enter a float: "))
+type3 = str(input("Enter a string: "))
+print(f"Integer: {type1}, Float: {type2}, String: {type3}")
