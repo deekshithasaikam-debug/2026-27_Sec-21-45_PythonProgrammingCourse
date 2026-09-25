82517893fe7b5abd82f2f804c91f6fdcbc51a791
deekshithasaikam-debug <deekshithasaikam@gmail.com>
2026-09-25 16:03:21 +0530

    Add files via upload
    Unit-2A_Task-4_Student_Grade_Elif_Ladder.py

---
+++ Unit-2A_Task-4_Student_Grade_Elif_Ladder.py
@@ -0,0 +1,15 @@
+# Student Grade Evaluation using elif ladder
+marks = float(input("Enter the student's marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 80:
+    grade = "B"
+elif marks >= 70:
+    grade = "C"
+elif marks >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"The student's grade is: {grade}")
